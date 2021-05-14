330549dbf8652c2c4a4e0ef68c33a57e2ce95fce
peeknpoke.net <contact@peeknpoke.net>
2021-05-14 11:14:08 +0300

    Setting basic layout

--- app/src/main/java/net/peeknpoke/apps/computeshaderexample/MainActivity.java
+++ app/src/main/java/net/peeknpoke/apps/computeshaderexample/MainActivity.java
@@ -2,13 +2,30 @@ package net.peeknpoke.apps.computeshaderexample;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int PICK_FROM_GALLERY = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onApplyShader(View view)
+    {
+
+    }
+
+    public void onImagePick(View view)
+    {
+        Intent galleryIntent = new Intent(Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        // Start the Intent
+        startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
     }
 }
